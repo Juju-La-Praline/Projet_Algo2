0bf67e05f256edc96e2b6bdc2e8324155810a1f8
khadidja-traore <60117869+khadidja-traore@users.noreply.github.com>
2020-11-28 00:54:42 +0100

    Update Parse1.py

--- ALGO2_code/Parse1.py
+++ ALGO2_code/Parse1.py
@@ -3,9 +3,12 @@ from GrilleC import *
 a='0.txt'
 
 def creer_grille2(z):# crée la grille a partir d'un fichier 
+    """ 
+    crée une grille à partir des valeurs du fichier z
     
-    tabic = []
-    tabil = []
+    """
+    tabic = []       # liste des sl des colonnes
+    tabil = []       # liste des sl des lignes
     N = 0
     M = 0
     tail_max_L = 0
@@ -17,28 +20,28 @@ def creer_grille2(z):# crée la grille a partir d'un fichier
     for ligne in fichier:
 
         i=i+1
-        if ligne[0]== '#':
+        if ligne[0]== '#':        #si la ligne commence par un #
             hashtag = True
-            N=i-1
+            N=i-1                 #nombre de lignes 
             i=0
         
-        elif hashtag:
+        elif hashtag:                          #cas des colonnes
             ligne_split=ligne.split()
-            Vtemp = [int(i) for i in ligne_split]
+            Vtemp = [int(i) for i in ligne_split]  #sequence d'entier d'une colonne
             
             if Vtemp != []:
                 tail_max_C=max(len(Vtemp),tail_max_C)
-            tabic+= [Vtemp]
+            tabic+= [Vtemp]                   #ajout à la liste des sl des colonnes
             
-        else:
+        else:                                      # cas des lignes
             ligne_split2 = ligne.split()
-            Vtemp2=[int(i) for i in ligne_split2]
+            Vtemp2=[int(i) for i in ligne_split2]  #sequence d'entier d'une ligne
 
-            if Vtemp2 != []:
+            if Vtemp2 != []: 
                 tail_max_L=max(len(Vtemp2),tail_max_L)
-            tabil+= [Vtemp2]
+            tabil+= [Vtemp2]                #ajout à la liste des sl des lignes
             
-    M=i       
+    M=i                                     #nombre de colonnes      
     fichier.close()
     g = Grille(N, M, tabil, tabic, tail_max_L+1, tail_max_C+3)
 
