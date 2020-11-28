d88191f8fce9f4e314f162c201e0177ea1057780
khadidja-traore <60117869+khadidja-traore@users.noreply.github.com>
2020-11-28 18:08:20 +0100

    Update and rename version_jsupSLM.py to version_jsupSLMO.py

--- ALGO2_code/version_jsupSLMO.py
+++ ALGO2_code/version_jsupSLMO.py
@@ -1,10 +1,11 @@
-def jsupSLMO(j, l, sl, ligne, D):# l taille de la sous sequence  , S la sous sequence
+def jsupSLMO(j, l, sl, ligne, D):
     #print("\n")
     
     #print("jsupSLMP(j=",j,", l=",l,", sl, ligne)")
     """
-    
-    
+    pour j un indice, une sous sequence sl de taille l, une ligne 
+    la fonction retourne True si une coloration de la ligne est possible, False sinon.
+    D est le dictionnaire des valeurs T(j,l), par exemple D[j,l] = True ou False.
     Une case à valeur 1 est blanche, 2 est noire et -1 est indéterminée.
     """
     
@@ -26,10 +27,10 @@ def jsupSLMO(j, l, sl, ligne, D):# l taille de la sous sequence  , S la sous seq
     elif j < sl[l-1] - 1:         #indice de j est trop petit, T(j,l) est faux
         return False
 
-    elif j == sl[l-1] - 1:       #j est assez grand si il y a un seul bloc
+    elif j == sl[l-1] - 1:       #j est assez grand que s'il reste un seul bloc
         if l == 1:
             for tti in ligne[0:j+1]:
-                if tti == 1:          #si une des cases est blanche, T(j,l) est faux
+                if tti == 1:          #si une des cases précédentes à j est blanche, T(j,l) est faux
                     return False
            
             return True
@@ -38,25 +39,25 @@ def jsupSLMO(j, l, sl, ligne, D):# l taille de la sous sequence  , S la sous seq
             
             return False
 
-    if ligne[j] == -1:
+    if ligne[j] == -1:               #si la case est indéterminée
         #print("ligne[j-1]==-1")
         
-        if ligne[j - sl[l-1]] == 2: 
+        if ligne[j - sl[l-1]] == 2:     #si cette case est noire 
                 #print("ligne[j-sl[l-1]-1] == 2:")
                 #print("false")
-                
-                if (j - 1 , l) not in D :
+                #alors la case ligne[j] est blanche sinon il y a une case noire de trop
+                if (j - 1 , l) not in D : 
             
                     D[j - 1 , l] = jsupSLMO(j - 1, l, sl, ligne, D)
                     
-                return D[j - 1 , l]
+                return D[j - 1 , l] 
                 
                 #return jsupSLM(j - 1, l, sl, ligne) # cas une case noire de trop par rapport a la taille
         
         
-        for i in ligne[j - sl[l-1]+1:j ] : # cas une case blanche dans sl dans la suite 
+        for i in ligne[j - sl[l-1]+1:j ] : #parcours des j - sl[l-1] cases précédent j  # cas une case blanche dans sl dans la suite 
             #print(i)
-            if i == 1 :
+            if i == 1 :            #si une des cases est blanche alors ligne[j] est blanche
                 #print("pas noire")
                 
                 if (j - 1 , l) not in D :
@@ -80,44 +81,43 @@ def jsupSLMO(j, l, sl, ligne, D):# l taille de la sous sequence  , S la sous seq
         
 
         #print("ne sais pas")        
-        #return jsupSLM(j - sl[l-1]-1, l-1, sl, ligne) or jsupSLM(j - 1, l, sl, ligne)   
+        #return jsupSLM(j - sl[l-1]-1, l-1, sl, ligne) or jsupSLM(j - 1, l, sl, ligne) 
+        #3 cas on test pour ligne[j] noire et ligne[j] blanche
         k = j - sl[l-1] - 1
         
         if ( k , l - 1) not in D :# if in D ? faire le or quand meme 
-            #print("appel", k, l-1)
+            #print("appel", k, l-1)  #considere que ligne[j] est noire
             D[k , l - 1] = jsupSLMO(j - sl[l-1]-1, l-1, sl, ligne, D)# le or ?
 
             if D[k, l - 1] == True :
                 return True
          
-        if (j-1, l) not in D:
+        if (j-1, l) not in D:  #considere que ligne[j] est blanche
             D[j - 1 , l] = jsupSLMO(j - 1, l, sl, ligne, D)
             
-
-        
         return D[k , l - 1] or D[j - 1 , l]
             
         
         
 
-    if ligne[j] == 1:#blanc
+    if ligne[j] == 1:      #si la case est blanche
 
         #○print("ligne[j-1]==1")
         #return jsupSLM(j - 1, l, sl, ligne) # case blanche
-        if (j - 1 , l) not in D :
+        if (j - 1 , l) not in D :  
             D[j - 1 , l] = jsupSLMO(j - 1, l, sl, ligne, D)
 
         return D[j - 1 , l]
         
 
 
-    if ligne[j] == 2:#noire
+    if ligne[j] == 2:           #si la case est noire
 
         #print("ligne[j-1]==2:")
-        for i in ligne[j - sl[l-1]+1:j ] : # cas une case blanche dans sl dans la suite 
-            if i == 1:
+        for i in ligne[j - sl[l-1]+1:j ] : # parcours des j - sl[l-1] cases avant j
+            if i == 1:                    #si une des cases est blanche
                 #print(i ,"false")
-                return False
+                return False               #le coloriage est impossible
             
         """if j-sl[l-1] +1 < 0: #cas pas la place de mettre les cases noires
             #print("j-sl[l-1] < 0:")
@@ -130,10 +130,10 @@ def jsupSLMO(j, l, sl, ligne, D):# l taille de la sous sequence  , S la sous seq
             return True
         else :"""
             
-        if ligne[j - sl[l-1]] == 2: 
+        if ligne[j - sl[l-1]] == 2:  #si cette case est noire
             #print("ligne[j-sl[l-1]-1] == 2:")
                 #print("false")
-            return False # cas une case noire de trop par rapport a la taille 
+            return False # coloriage impossible car une case noire de trop 
         else :
             k = j - sl[l-1] - 1
             if (k, l - 1) not in D :
